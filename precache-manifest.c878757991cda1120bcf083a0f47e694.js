de1e1733a71619f926f48728711bcdf9e9bac752
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-23 00:47:38 -0300

    Updates

---
+++ precache-manifest.c878757991cda1120bcf083a0f47e694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8185a11e43e051df3590d77f9f82c9d1",
+    "url": "/react-postgre-todo/index.html"
+  },
+  {
+    "revision": "8358bd5f2767510ff45c",
+    "url": "/react-postgre-todo/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e6ab986436dc86857a27",
+    "url": "/react-postgre-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8358bd5f2767510ff45c",
+    "url": "/react-postgre-todo/static/js/2.96ecfb49.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-postgre-todo/static/js/2.96ecfb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ab986436dc86857a27",
+    "url": "/react-postgre-todo/static/js/main.765fdf18.chunk.js"
+  },
+  {
+    "revision": "6db5976f08d5bbf10919",
+    "url": "/react-postgre-todo/static/js/runtime-main.e7beabf7.js"
+  }
+]);
